31036188dcd2d793f55dba4217c4f4f6097ee0ab
Vladyslav Lutsenko <vladlutsenko.2011@gmail.com>
2023-04-02 17:48:02 +0300

    implemented proper jumping mechanic (via ground check)

--- Assets/Scripts/Player/PlayerEntity.cs
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -12,37 +12,26 @@ namespace Player
         [SerializeField] private float _horizontalSpeed;
         [SerializeField] private bool _faceRight;
 
-        //[Header("VerticalMovement")]
-        //[SerializeField] private float _verticalSpeed;
-        //[SerializeField] private float _minSize;
-        //[SerializeField] private float _maxSize;
-        //[SerializeField] private float _minVerticalPosition;
-        //[SerializeField] private float _maxVerticalPosition;
-
         [Header("Jump")]
         [SerializeField] private float _jumpForce;
-        [SerializeField] private float _gravityScale;
+        [SerializeField] private float _gravityScale;       
+        [SerializeField] private Transform _groundCheck;
+        [SerializeField] private float _groundCheckRadius;
+        [SerializeField] private LayerMask _groundLayer;
 
         private Rigidbody2D _rigidbody;
+        private bool _isOnGround;
 
-        //private float _sizeModificator;
-        [SerializeField]  private bool _isJumping;
-        private float _startJumpVerticalPosition;
 
         // Start is called before the first frame update
         private void Start()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
-
-            //float positionDifference = _maxVerticalPosition - _minVerticalPosition;
-            //float sizeDifference = _maxSize - _minSize;
-            //_sizeModificator = sizeDifference / positionDifference;
-            //UpdateSize();
         }
 
         private void Update()
         {
-            if (_isJumping)
+            if (!_isOnGround)
             {
                 UpdateJump();
             }
@@ -50,15 +39,34 @@ namespace Player
 
         public void Jump()
         {
-            if (_isJumping)
+            if (!IsOnGround())
             {
                 return;
             }
-
-            _isJumping = true;
+            _isOnGround = true;
             _rigidbody.AddForce(Vector2.up * _jumpForce);
             _rigidbody.gravityScale = _gravityScale;
-            _startJumpVerticalPosition = transform.position.y;
+        }
+
+        private bool IsOnGround()
+        {
+            return Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _groundLayer);
+        }
+
+        private void UpdateJump()
+        {
+            _isOnGround = IsOnGround();
+
+            if (_isOnGround)
+            {
+                ResetJump();
+                return;
+            }
+        }
+
+        private void ResetJump()
+        {
+            _isOnGround = false;
         }
 
         public void MoveHorizontally(float direction)
@@ -68,32 +76,6 @@ namespace Player
             velocity.x = direction * _horizontalSpeed;
             _rigidbody.velocity = velocity;
         }
-
-        //public void MoveVertically(float direction)
-        //{
-        //    if (_isJumping)
-        //        return;
-
-        //    Vector2 velocity = _rigidbody.velocity;
-        //    velocity.y = direction * _verticalSpeed;
-        //    _rigidbody.velocity = velocity;
-
-        //    if (direction == 0)
-        //    {
-        //        return;
-        //    }
-
-        //    float verticalPosition = Mathf.Clamp(transform.position.y, _minVerticalPosition, _maxVerticalPosition);
-        //    _rigidbody.position = new Vector2(_rigidbody.position.x, verticalPosition);
-        //    UpdateSize();
-        //}
-
-        //private void UpdateSize()
-        //{
-        //    float verticalDelta = _maxVerticalPosition - transform.position.y;
-        //    float currentSizeModificator = _minSize + _sizeModificator * verticalDelta;
-        //    transform.localScale = Vector2.one * currentSizeModificator;
-        //}
 
         private void SetDirection(float direction)
         {
@@ -109,23 +91,6 @@ namespace Player
             transform.Rotate(0, 180, 0);
             _faceRight = !_faceRight;
         }
-
-        private void UpdateJump()
-        {
-            if(_rigidbody.velocity.y < 0 && _rigidbody.position.y <= _startJumpVerticalPosition)
-            {
-                ResetJump();
-                return;
-            }
-
-
-        }
-
-        private void ResetJump()
-        {
-            _isJumping = false;
-            //_rigidbody.position = new Vector2(_rigidbody.position.x, _startJumpVerticalPosition);
-            //_rigidbody.gravityScale = 0;
-        }
+        
     }
 }
