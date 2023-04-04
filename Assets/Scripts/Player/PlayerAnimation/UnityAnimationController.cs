87391c4e45b14f44eda50c2bb43449a52169d4c4
Vladyslav Lutsenko <vladlutsenko.2011@gmail.com>
2023-04-04 20:26:04 +0300

    added attack animation and scripts to work with it (and other anims)

---
+++ Assets/Scripts/Player/PlayerAnimation/UnityAnimationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Player.PlayerAnimation
+{
+    [RequireComponent(typeof(Animator))]
+    public class UnityAnimationController: AnimationController
+    {
+        private Animator _animator;
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        protected override void PlayAnimation(AnimationType animationType)
+        {
+            _animator.SetInteger(nameof(AnimationType), (int)animationType);
+        }
+    }
+}
